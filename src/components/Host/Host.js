5333bea1d30b641dcdcc6dafc594f45dc301c87f
Brian Lowe <mynameisboy@gmail.com>
2019-10-29 15:09:57 -0600

    conditional render host

--- src/components/Host/Host.js
+++ src/components/Host/Host.js
@@ -1,27 +1,61 @@
-import React, {Component} from 'react'
+import React, {Component} from "react";
 
 class Host extends Component {
-    constructor() {
-        super()
-        this.state = {
-            name: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      code: null,
+      submittedName: false
+    };
+  }
 
-    handleChange(value) {
-        this.setState({
-            name: value
-        })
-    }
+  componentDidMount() {
+    this.codeGenerator();
+  }
 
-    render() {
-        return (
-            <div className="host-container">
-                <header className="title">Get Started</header>
-                <input onChange={e => this.handleChange(e.target.value)} type="text" className="name"/>
-            </div>
-        )
-    }
+  handleChange(value) {
+    this.setState({
+      name: value
+    });
+  }
+
+  codeGenerator() {
+    const code = Math.ceil(Math.random() * 10000);
+    this.setState({
+      code: code
+    });
+  }
+
+  submit() {
+    this.setState({
+      submittedName: true
+    });
+  }
+
+  render() {
+    return (
+      <div className="host-container">
+        {!this.state.submittedName ? (
+          <>
+            <header className="title">Get Started</header>
+            <input
+              onChange={e => this.handleChange(e.target.value)}
+              type="text"
+              className="name"
+            />
+            <button onClick={() => this.submit()} className="submit">
+              submit
+            </button>
+          </>
+        ) : (
+          <>
+            <h3 className="code">{this.state.code}</h3>
+          </>
+        )}
+      </div>
+    );
+  }
 }
 
-export default Host
+export default Host;
