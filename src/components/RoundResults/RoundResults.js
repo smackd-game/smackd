186b4c78a0c7bb38e5e6b200d209cf83a52e42d6
Tate Stone <tatejamesstone@gmail.com>
2019-11-01 10:36:33 -0600

    Fixed basic styling and moved state adn functions to parent

--- src/components/RoundResults/RoundResults.js
+++ src/components/RoundResults/RoundResults.js
@@ -1,33 +1,30 @@
 import React, {Component} from 'react'
+import './_roundResults.scss'
 
 class RoundResults extends Component {
     constructor() {
         super()
         this.state = {
-            isReady: false
+           
         }
     }
-    clickedReady = () => {
-        this.setState({
-            isReady: true
-        })
-        console.log(this.state)
-    }
+    
 
     render() {
         return (
             <div className="round-results-container">
 
-            {!this.state.isReady ? (
+            {!this.props.isReady ? (
                 <>
                     <h2>Round {this.props.round} Results</h2>
                 <p>We will map the list of players with their round points and total points</p>
-                <button onClick={() => this.clickedReady()} >Ready for next round?</button>
+                <button className='landing-button' onClick={() => this.props.clickedReadyFN()} >Ready for next round?</button>
                 </>
                 ) : (
                 <>
                     <h2>Round {this.props.round} Results</h2>
                 <p>We will map the list of players with their round points and total points</p>
+                <p>Waiting on other players to tap "Ready"</p>
                 </>
                 )}
                 
