f077a14afcb31bf341941b747ff9422d74c58ed3
Brian Lowe <mynameisboy@gmail.com>
2019-11-04 11:18:40 -0700

    answers are going through

--- server/server.js
+++ server/server.js
@@ -109,9 +109,13 @@ io.on("connection", socket => {
   });
 
   socket.on("update answers", data => {
-    console.log(data)
     io.to(data.room).emit("update answers", data);
     console.log("emitting updated answers");
+  });
+
+  socket.on("send answer", data => {
+    io.to(data.room).emit("receive answer", data);
+    console.log("sending answers");
   });
 
 
