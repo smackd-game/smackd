7c477668112d33a63a4e794742a92871b51da760
Tate Stone <tatejamesstone@gmail.com>
2019-11-06 16:05:36 -0700

    Some styling

--- src/components/RoundResults/RoundResults.js
+++ src/components/RoundResults/RoundResults.js
@@ -7,24 +7,30 @@ class RoundResults extends Component {
         let players = this.props.players
         let arr = players.filter(el => !el.readyForNextRound)
         let arentReady = arr.map((el, i) => {
-            return <p key={i}>{el.name}</p>
+            return <p className='round-results-arent-ready' key={i}>{el.name}</p>
         })
         let playersResults = players.map((el, i) => {
-            return <p key={i}>{el.name} Round Points: {el.roundPoints === null ? 0 : el.roundPoints} Total Points: {el.totalPoints === null ? 0 : el.totalPoints}</p>
+            return <p className='round-results-players' key={i}>{el.name} | {el.roundPoints === null ? 0 : el.roundPoints} | {el.totalPoints === null ? 0 : el.totalPoints}</p>
         })
         return (
             <div className="round-results-container">
 
             {!this.props.isReady ? (
                 <>
-                <h2>Round {this.props.round} Results</h2>
+                <h2 className='round-title'>Round {this.props.round} Results</h2>
+                <div className="players-results">
+                <h4>Name | Round Points | Total Points</h4>
+
                 {playersResults}
-                <button className='landing-button' onClick={() => this.props.clickedReadyFN()} >Ready for next round?</button>
+
+                </div>
+                <button className='landing-button' onClick={() => this.props.clickedReadyFN()} >Next Round</button>
                 </>
                 ) : (
                 <>
                 <h2>Round {this.props.round} Results</h2>
                 <p>Still waiting on</p>
+                
                 {arentReady}
                 <p>We're still waiting on a few players to click the Ready button. Once they've clicked it, we'll move to the next round, tell them to hurry and do it.</p>
                 </>
